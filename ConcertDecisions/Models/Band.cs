9e4dc5764362575f2437ab5ffd13aabda56ec753
luis felipe amador bravo <luis_felipe_amador@hotmail.com>
2023-06-26 19:30:13 -0600

    Add models of Band and Requirements

---
+++ ConcertDecisions/Models/Band.cs
@@ -0,0 +1,11 @@
+﻿namespace ConcertDecisions.Models
+{
+    public class Band
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        //Value from 0 to 100 of how much I want to go to a concert of this band
+        public int Fan_Level { get; set; }
+    }
+}
---
+++ ConcertDecisions/Models/Requirements.cs
@@ -0,0 +1,24 @@
+﻿namespace ConcertDecisions.Models
+{
+    public class Requirements
+    {
+        public int Id { get; set; }
+
+        //Event Type (Concert, festival)
+        public string Event_Type { get; set; } = string.Empty;
+
+        //Excelent Points to go to an Event, Dont miss it
+        public int Excelent_Points { get; set; }
+
+        //Good Points to go to an Event
+        public int Good_Points { get; set; }
+
+        //Regular points to go to an Event
+        public int Regular_Points { get; set; }
+
+        //Minimum of points to go to an Event
+        public int Minimum_Points { get; set; }
+
+
+    }
+}
